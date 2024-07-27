1c31f1fcd6628f1b413937197121e5b87e4f1fec
samatsum <zunandkun@gmail.com>
2024-07-28 00:12:13 +0900

    samatsum_frontend修正03

--- webapp/frontend/src/app/error.tsx
+++ webapp/frontend/src/app/error.tsx
@@ -1,19 +1,25 @@
+// "use client" ディレクティブは、このコンポーネントがクライアントサイドでレンダリングされることを示します。
 "use client";
 
 import { useEffect } from "react";
 
+// エラーハンドリング用のカスタムコンポーネント
 export default function Error({ error }: { error: Error & { digest?: string } }) {
+  // useEffectフックを使用して、エラーが発生した際にコンソールにエラーメッセージをログ出力
   useEffect(() => {
     console.error(error);
-  }, [error]);
+  }, [error]); // errorが変更されるたびにこのエフェクトが実行される
 
   return (
     <div id="error-page">
+      {/* ユーザーフレンドリーなエラーメッセージを表示 */}
       <h1>Oops!</h1>
       <p>Sorry, an unexpected error has occurred.</p>
       <p>
+        {/* 実際のエラーメッセージを表示 */}
         <i>{error.message}</i>
       </p>
     </div>
   );
 }
+
